dd2746d5e2075480f077fc74c77dc6cc11138c3c
Wahyu Aprilliandhika <wahyuandhika910@gmail.com>
2023-11-08 14:55:28 +0700

    test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/biskuatcoklat/module-hello
+
+go 1.20
+
+require github.com/biskuatcoklat/module-go-simple v1.0.0 // indirect
---
+++ sayHello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	module_go "github.com/biskuatcoklat/module-go-simple"
+)
+
+func main() {
+	fmt.Println(module_go.SayHai())
+}
